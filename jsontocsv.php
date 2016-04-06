0f0ad8593277c5c670b442829759f73e416046e5
Axel Schelfhout <axelschelfhout@gmail.com>
2016-04-06 15:38:20 +0200

    Json is being converted to CSV correctly now

--- jsontocsv.php
+++ jsontocsv.php
@@ -1,77 +1,159 @@
 <?php
 
+namespace jsontocsv;
+
 /**
  * Class JsonToCSV
  *
- * @author Axel Schelfhout
+ * @author Axel Schelfhout <axelschelfhout@gmail.com>
  */
 class JsonToCSV
 {
 
-    public $columns;
+    private $columns;
+    private $csv;
+    private $rows;
+
 
     /**
-     * JsonToCSV constructor.
+     * Text
+     *
      * @param $file
-     * @param bool $skipFirstDimension
+     * @param string $csvFileName
+     * @return void
      */
-    public function __construct($file, $skipFirstDimension = false)
+    public function convert($file, $csvFileName = '')
     {
-        $fileContent = file_get_contents($file);
+        $fileContent = $this->getFileContent($file);
 
         // Make it a readable array, so we can use it.
         $data = json_decode($fileContent, true);
 
-        // If the first dimension of the data are entry ID's. We should skip the first, and go directly into the second.
-        // This is where the data is.
-        if ($skipFirstDimension) {
-            $data = $data[0];
-        }
-        $this->columnSearch($data);
+        $this->columnSearch($data); // Find the column names.
+        $this->fixRows(); // add all columns to all rows
 
-        return $this->getColumns();
+        $this->createCSV($csvFileName);
     }
 
     /**
+     * Text
+     *
+     * @param $csvFileName
+     */
+    private function createCSV($csvFileName)
+    {
+        if (strlen($csvFileName) < 1) {
+            $csvFileName = 'jsontocsv.csv';
+        }
+        $this->csv = fopen($csvFileName, 'w');
+        $this->addHeader();
+        $this->addData();
+
+        fclose($this->csv);
+    }
+
+    /**
+     * Text
+     *
+     * @return void
+     */
+    private function addData()
+    {
+        foreach ($this->rows as $row) {
+            fputcsv($this->csv, $row);
+        }
+    }
+
+
+    /**
+     * Text
+     *
+     * @return void
+     */
+    private function addHeader()
+    {
+        fputcsv($this->csv, $this->getColumns()); // First add the column names
+    }
+
+    /**
+     * Return's the content of the provided file.
+     *
+     * @param $file
+     * @return string
+     */
+    private function getFileContent($file)
+    {
+        $fileHandle = fopen($file, 'r');
+        $fileContent = fread($fileHandle, filesize($file));
+        fclose($fileHandle);
+        return $fileContent;
+    }
+
+    /**
+     * Text
+     *
      * @param $data
      * @param string $parent
+     * @param null $topKey
      */
-    public function columnSearch($data, $parent = '')
+    public function columnSearch($data, $parent = '', $topKey = null)
     {
-        foreach ($data as $key => $value) {
-            if (!is_numeric($key)) {
-                if (is_array($value)) {
-                    if (strlen($parent) > 0) {
-                        $key = $parent.'_'.$key;
-                    }
-                    $this->columnSearch($value, $key);
-                }
-                else {
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                if (!is_numeric($key)) {
+                    if (is_array($value)) {
+                        if (strlen($parent) > 0) {
+                            $key = $parent.'_'.$key;
+                        }
+                        $this->columnSearch($value, $key, $topKey);
+                    } else {
+                        if (strlen($parent) > 0) {
+                            $newKey = $parent.'_'.$key;
+                            $this->columns[] = $newKey;
 
-                    if(strlen($parent)>0){
-                        $newKey = $parent.'_'.$key;
-                        $this->columns[] = $newKey;
+                            $this->rows[$topKey][$newKey] = strval($value); //
+                        } else {
+                            $this->columns[] = $key;
+
+                            $this->rows[$topKey][$key] = strval($value); //
+                        }
                     }
-                    else {
-                        $this->columns[] = $key;
-                    }
+                } else {
+                    $this->columnSearch($value, $parent, $key);
                 }
-            } else {
-                $this->columnSearch($value, $parent);
             }
         }
     }
 
     /**
+     * Text
+     *
+     * @return void
+     */
+    public function fixRows()
+    {
+        foreach ($this->rows as $key => $row) {
+            foreach ($this->columns as $col) {
+                if (!array_key_exists($col, $row)) {
+                    $this->rows[$key][$col] = '';
+                }
+            }
+            ksort($this->rows[$key]);
+        }
+    }
+
+    /**
+     * Text
+     *
      * @return mixed
      */
     public function getColumns()
     {
         if (!empty($this->columns)) {
-            return $this->columns;
+            $columns = array_unique($this->columns);
+            sort($columns);
+            return $columns;
         }
         return null;
     }
-
 }
-
